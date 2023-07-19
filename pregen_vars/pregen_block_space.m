a3346f2acf22b3e0e6777725f1389f447ee02420
prashantig25 <75947365+prashantig25@users.noreply.github.com>
2023-07-19 15:13:21 +0200

    generate files for training practice blocks

--- pregen_vars/pregen_block_space.m
+++ pregen_vars/pregen_block_space.m
@@ -1,15 +1,17 @@
 % INITIALISE BLOCK VARS (don't change the combination)
-% explain these with examples and context to the task
-subj_state_crystal = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]; % state 
-subj_cond = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2]; % condition
-subj_cong = [0,0,1,1,0,0,1,1,1,1,0,0,1,1,0,0]; % congruence
-subj_left = [0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1]; % mu for left/right planet?
-subj_planet_pos = [1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0]; % positions of planet
-subj_state_prob = [0.3,0.1,0.3,0.1,0.3,0.1,0.3,0.1,0.7,0.9,0.7,0.9,0.7,0.9,0.7,0.9]; % probability of state = 1
-
+subj_state_crystal = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1]; % state 
+subj_cond = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,3,3,3,3,3,3,3,3]; % condition
+subj_cong = [0,0,1,1,0,0,1,1,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0]; % congruence
+subj_left = [0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0]; % mu for left/right planet?
+subj_planet_pos = [1,0,0,1,1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,0,1]; % positions of planet
+subj_state_prob = [0.3,0.1,0.3,0.1,0.3,0.1,0.3,0.1,0.7,0.9,0.7,0.9,0.7,0.9,0.7,0.9,0.3,0.7,0.7,0.3,0.7,0.3,0.3,0.7]; % probability of state = 1
+subj_state_prob_training = [0.3,0,0.3,0,0.3,0,0.3,0,0.7,1,0.7,1,0.7,1,0.7,1,0,1,1,0,1,0,0,1]; % probability of state = 1 for training blocks
+con_high = [repelem(0.1,1,16),repelem(0.45,1,8)];
+con_low = [repelem(0,1,16),repelem(0.35,1,8)];
+training = [repmat([0,1],1,8),ones(1,1,8)];
 
 num_subjs = 1; % number of subjects
-blocks = 1:16; % number of blocks (for training as well??)
+blocks = 1:24; % number of blocks
 
 for n = 1:num_subjs
      
@@ -18,16 +20,20 @@ for n = 1:num_subjs
      mkdir(folder) 
 
      % PREGEN VARS FOR EVERY BLOCK
-     for b = 1:length(blocks)
+     for b = 17:length(blocks)
 
         choice_mu = repelem(0,length(subj_cong),1);
-        choice = repelem(1,length(subj_cong),1); % what's going on here???
-      
+        choice = repelem(1,length(subj_cong),1);
+        
         num_trials = 20; % number of trials
-        s1_prob = subj_state_prob(b); % proportion of trials with s = 1
+        if training(b) == 0
+            s1_prob = subj_state_prob(b); % proportion of trials with s = 1
+        else
+            s1_prob = subj_state_prob_training(b);
+        end
         avg_vis = 0.5; % avg visibility
-        contrast_high = 0.1; % highest level of contrast difference
-        contrast_low = 0; % lowest level of contrast difference
+        contrast_high = con_high(b); % highest contrast level
+        contrast_low = con_low(b); % lowest contrast level
         state_crystal = subj_state_crystal(b); % more probable state of block
         cond = subj_cond(b); % condition of block
         cong = subj_cong(b); % congruence of block
@@ -53,13 +59,27 @@ for n = 1:num_subjs
             left,planet_pos,'VariableNames',{'state','condiff','con_left','con_right','state_crystal',...
             'congruence','condition','row_id','left','planet_pos'});
 
+        % CORRECT RESPONSE FOR TRAINING
+        if training(b) == 1
+            for t = 1:height(tbl)
+                tbl.corr_num(t) = 1-tbl.state(t);
+                if tbl.corr_num(t) == 0
+                    tbl.corr_resp(t) = 'left';
+                else
+                    tbl.corr_resp(t) = 'right';
+                end
+            end
+        end
+
         B = tbl(randperm(20), :);
 
         % NAME THE FILE
         if cond == 1
             cond_name = '\mix_';
-        else
+        elseif cond == 2
             cond_name = '\perc_';
+        else
+            cond_name = '\rew_';
         end
         
         if state_crystal(1) == 1
@@ -82,6 +102,6 @@ for n = 1:num_subjs
         
         % SAVE THE TABLE
         filename_tbl = strcat(folder,cond_name,state_name,left_name,cong_name,'.xlsx');
-        writetable(B,filename_tbl);
+%         writetable(B,filename_tbl);
      end
 end
