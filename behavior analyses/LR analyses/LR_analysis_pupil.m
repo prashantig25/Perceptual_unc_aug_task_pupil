10668f7bc4f57904dbb62f53312140b9f5bf8e3d
Prashanti Ganesh <prashantig25@gmail.com>
2024-07-29 15:09:52 +0000

    updated LR analysis script

--- behavior analyses/LR analyses/LR_analysis_pupil.m
+++ behavior analyses/LR analyses/LR_analysis_pupil.m
@@ -14,10 +14,6 @@ preprocess_obj.compute_state_dep_pe(); % compute state dependent PE and UP
 preprocess_obj.compute_ru(); % reward uncertainty
 preprocess_obj.compute_confirm(); % confirming outcome
 norm_condiff = preprocess_obj.compute_normalise(abs(preprocess_obj.data.con_diff)); % normalised contrast difference
-% norm_patchphasic = preprocess_obj.compute_normalise(preprocess_obj.data.patch_phasic);
-% norm_patchtonic = preprocess_obj.compute_normalise(preprocess_obj.data.patch_tonic);
-% norm_fbphasic = preprocess_obj.compute_normalise(preprocess_obj.data.fb_phasic);
-% norm_fbtonic = preprocess_obj.compute_normalise(preprocess_obj.data.fb_tonic);
 preprocess_obj.add_splithalf(); % add splithalf variable
 if preprocess_obj.space_pavlovian == 0
     preprocess_obj.add_saliencechoice(); % add salience chhoice
@@ -29,29 +25,13 @@ preprocess_obj.add_vars(preprocess_obj.data.ru,'reward_unc');
 preprocess_obj.add_vars(preprocess_obj.data.confirm_rew,'pe_sign');
 
 % SAVE PREPROCESSED FILE
-writetable(preprocess_obj.data,'preprocessed_lr_pupil.xlsx');
-writetable(preprocess_obj.data(preprocess_obj.data.pe ~= 0, :),'preprocessed_lr_pavlovian_no_zerope.xlsx');
-
-% SAVE FILES SEPARATELY FOR GROUPED REGRESSION
-grouped = 0; % 1 if files need to be saved separately for grouped regression
-if grouped == 1
-    data = readtable("preprocessed_lr_pupil_no_zerope.xlsx");
-    condiff = data.con_diff;
-    bin_edges = prctile(condiff, 0:50:100); % Calculate percentile edges
-    bins = discretize(data.con_diff, bin_edges);
-    data.bins = bins;
-    writetable(data(data.bins == 1,:),'preprocessed_subj_condiffbin1.xlsx');
-    writetable(data(data.bins == 2,:),'preprocessed_subj_condiffbin2.xlsx');
-end
-% writetable(data(data.con_diff >= 0.02,:),'preprocessed_subj_no_condiffbin1.xlsx');
+safe_saveall('preprocessed_lr_pupil.xlsx',preprocess_obj.data)
+safe_saveall('preprocessed_lr_pupil_no_zerope.xlsx',preprocess_obj.data(preprocess_obj.data.pe ~= 0, :));
 %%
 % FIT THE MODEL
 lr_analysis = lr_analysis_obj();
 [betas_all,rsquared_full,residuals_all,coeffs_name,posterior_up_subjs] = lr_analysis.get_coeffs(@fitlm);
-% p_vals = lr_analysis.p_vals;
 
 % SAVE DATA 
-save("betas_abs_condiff","betas_all"); % save betas
-save("residuals_signed","residuals_all"); % residuals
-save("rsquared_abs.mat","rsquared_full"); % save r-squared values
-save("post_signedUP_predict.mat","posterior_up_subjs"); %
+safe_saveall("betas_signed.mat",betas_all); % save betas
+safe_saveall("post_signedUP_predict.mat",posterior_up_subjs); 
