0ba2174464d3bd604b62c72036473933ce4ac8aa
Prashanti Ganesh <prashantig25@gmail.com>
2024-07-29 15:10:48 +0000

    preprocessing scripts

--- behavior analyses/LR analyses/preprocess_vars.m
+++ behavior analyses/LR analyses/preprocess_vars.m
@@ -2,14 +2,14 @@ classdef preprocess_vars < handle
 % PREPROCESS_VARS is a superclass that specifies variables for the preprocessing of
 % behavioural data, to compute regressors for model based analyses.
     properties
-        filename = 'preprocessed_lr_pupil.xlsx'; % path of file with behavioural data
+        filename = 'pupilbehv_all.xlsx'; % path of file with behavioural data
         agent = 0 % if analysis is being run for normative agent
-        pupil = 0; % if analysis is being run for behavioural data from pupil study
+        pupil = 1; % if analysis is being run for behavioural data from pupil study
         space = 0; % if analysis is being run for space task data (instrumental version)
         space_pavlovian = 0; % for only space pavlovian task
-        online = 1; % if analysis is being run on online dataset
+        online = 0; % if analysis is being run on online dataset
         removed_cond = [3] % experimental condition number that is to be excluded during analysis
-        num_subjs = 49; % number of participants
+        num_subjs = 47; % number of participants
         absolute_lr = 0; % if analysis should be fit to absolute UPs and absolute PEs 
         data % table with behavioural data
         mu % reported contingency parameter/reward probability
