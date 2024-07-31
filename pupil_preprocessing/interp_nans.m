2dce60a91f29e41d45d8e07098b8d6cb69adf851
Prashanti Ganesh <prashantig25@gmail.com>
2024-07-31 15:01:00 +0000

    added function documentation

--- pupil_preprocessing/interp_nans.m
+++ pupil_preprocessing/interp_nans.m
@@ -1,21 +1,34 @@
 function [p_out,p_nans] = interp_nans(p_in,badsmps)
-% todo: document properly!
 
-% make the pupil NaN within all identified blink windows
-for b = 1:size(badsmps,1),
-    p_in(badsmps(b,1):badsmps(b,2)) = NaN;
+    % function INTERP_NANS innterpolates NaN values in the input data 
+    % within specified blink windows.
+    %
+    % INPUTS:
+    %   p_in: Input data vector containing pupil measurements.
+    %   badsmps: Matrix specifying blink windows, where each row contains
+    %             start and end indices of a blink window (size Nx2).
+    %
+    % OUTPUT:
+    %   p_out: Output data vector with NaN values interpolated and extrapolated.
+    %   p_nans: Indices of NaN values in the input data vector after setting
+    %            NaNs within blink windows.
+    
+    % REPLACE BLINKS WITH NaNs
+    for b = 1:size(badsmps,1),
+        p_in(badsmps(b,1):badsmps(b,2)) = NaN;
+    end
+    
+    % STORE INDICES OF NaNs
+    p_nans = find(isnan(p_in));
+    
+    % INTERPOLATE
+    p_in(isnan(p_in)) = interp1(find(~isnan(p_in)), ...
+        p_in(~isnan(p_in)), find(isnan(p_in)), 'linear');
+    
+    % EXTERPOLATE ENDS
+    p_in(isnan(p_in)) = interp1(find(~isnan(p_in)), ...
+        p_in(~isnan(p_in)), find(isnan(p_in)), 'nearest', 'extrap');
+    
+    % STORE OUTPUT
+    p_out = p_in;
 end
-
-% store indices of nans
-p_nans = find(isnan(p_in));
-
-% interpolate linearly
-p_in(isnan(p_in)) = interp1(find(~isnan(p_in)), ...
-    p_in(~isnan(p_in)), find(isnan(p_in)), 'linear');
-
-% also extrapolate ends
-p_in(isnan(p_in)) = interp1(find(~isnan(p_in)), ...
-    p_in(~isnan(p_in)), find(isnan(p_in)), 'nearest', 'extrap');
-
-% make output
-p_out = p_in;
